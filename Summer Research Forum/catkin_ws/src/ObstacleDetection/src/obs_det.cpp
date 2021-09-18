7bc788c8545fedcf312ffafec775b8e10367d157
ndinh369 <56991477+ndinh369@users.noreply.github.com>
2021-09-18 21:31:47 +1000

    Update obs_det.cpp

--- Summer Research Forum/catkin_ws/src/ObstacleDetection/src/obs_det.cpp
+++ Summer Research Forum/catkin_ws/src/ObstacleDetection/src/obs_det.cpp
@@ -40,37 +40,6 @@ class lidarReact {
         }
 };
 
-class ultraReact {
-    public:
-        // Initialise publisher and subscriber nodes
-        ros::NodeHandle n;
-        ros::Publisher move_pub;
-        ros::Subscriber ultra_sub; 
-
-        // Constructor
-        ultraReact(){
-            move_pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
-            ultra_sub = n.subscribe<sensor_msgs::PointCloud>("/RosAria/sonar", 1000, &ultraReact::callback, this); 
-        } 
-
-        void callback (const sensor_msgs::PointCloud::ConstPtr& ultraMsg) {
-            geometry_msgs::Twist move_msg;
-            //std::cout << "hello" << std::endl;
-            double dist = std::sqrt(std::pow(ultraMsg->points[0].x,2)+std::pow(ultraMsg->points[0].y,2));
-            if (dist < 0.5) {
-                std::cout << "distance is " << dist << " less than 2m" << std::endl;
-                move_msg.linear.x = 0; // in m/s
-                move_msg.linear.y += 1;
-                move_msg.linear.z = 0;
-                std::cout << "obstacle detected" << std::endl;
-            } else {
-                std::cout << "no obstacle detected " << dist << std::endl;
-                move_msg.linear.x += 0.1;
-            }
-            move_pub.publish(move_msg); 
-        }
-};
-    
 int main (int argc, char **argv) {
     // Initialise ROS with unique node name "reactLidar"
     ros::init(argc, argv, "reactLidar");
